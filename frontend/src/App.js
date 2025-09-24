82821b2c3bba5b9ee73d860c58c20dcee89e428d
Dimitri Lagouris <dimitrilagouris@gmail.com>
2025-09-24 17:06:07 +1000

    toggle markets page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -71,28 +71,28 @@ function App() {
   return (
     <div className="App">
       <Header />
-        <Login />
-      {/*<main className="main-content">*/}
-      {/*  <div className="markets-container">*/}
-      {/*    <Market*/}
-      {/*      marketName="Who will be elected USU President?"*/}
-      {/*      events={presidentialPrediction}*/}
-      {/*      volume="$12,840"*/}
-      {/*    />*/}
+        {/*<Login />*/}
+      <main className="main-content">
+        <div className="markets-container">
+          <Market
+            marketName="Who will be elected USU President?"
+            events={presidentialPrediction}
+            volume="$12,840"
+          />
 
-      {/*    <Market*/}
-      {/*      marketName="How will transport to the new campus be solved?"*/}
-      {/*      events={busRouteExpansion}*/}
-      {/*      volume="$5,210"*/}
-      {/*    />*/}
+          <Market
+            marketName="How will transport to the new campus be solved?"
+            events={busRouteExpansion}
+            volume="$5,210"
+          />
 
-      {/*    <Market*/}
-      {/*      marketName="Who will win the next inter-varsity grand final?"*/}
-      {/*      events={grandFinalWinner}*/}
-      {/*      volume="$7,690"*/}
-      {/*    />*/}
-      {/*  </div>*/}
-      {/*</main>*/}
+          <Market
+            marketName="Who will win the next inter-varsity grand final?"
+            events={grandFinalWinner}
+            volume="$7,690"
+          />
+        </div>
+      </main>
     </div>
   );
 }
