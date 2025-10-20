4122245b6217ad68dc634b42201c4ef67b2f778a
Dimitri Lagouris <dimitrilagouris@gmail.com>
2025-10-20 16:14:11 +1100

    unsuspend_users function

--- djangoProject/views/admin_views.py
+++ djangoProject/views/admin_views.py
@@ -66,5 +66,23 @@ def suspend_user(request, user_id):
     
     return Response({'message': 'User suspended successfully'}, status=200)
 
-
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+@admin_required
+def unsuspend_user(request, user_id):
+    try:
+        user = Profiles.objects.get(pk=user_id)
+    except Profiles.DoesNotExist:
+        return Response({'error': 'User not found'}, status=404)
+    
+    if user.is_active:
+        return Response({'error': 'User already active'}, status=400)
+    
+    user.is_active = True
+    user.save()
+    
+    AdminActions.objects.create(user=request.user,
+        description=f"Unsuspended: {user.username} ({user.email})")
+    
+    return Response({'message': 'User reactivated successfully'}, status=200)
 
