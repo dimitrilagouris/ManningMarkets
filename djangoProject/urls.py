ab29d247e9838d6184885926749444af01459430
Dimitri Lagouris <dimitrilagouris@gmail.com>
2025-11-05 15:58:24 +1100

    droc4247 | waive if settings.debug block from urls

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -82,6 +82,3 @@ urlpatterns = [
     re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
